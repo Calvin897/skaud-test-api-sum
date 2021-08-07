30ae4f77f159eacfb0e39161a550b3e860e4a4b7
calvin tribelhorn <calvintribby@gmail.com>
2021-08-07 20:35:40 +0200

    completed api

--- app.js
+++ app.js
@@ -1,26 +1,49 @@
-var createError = require('http-errors');
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
+var createError = require("http-errors");
+var express = require("express");
+var path = require("path");
+var cookieParser = require("cookie-parser");
+var logger = require("morgan");
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+var indexRouter = require("./routes/index");
+var usersRouter = require("./routes/users");
 
 var app = express();
 
-// view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
+app.use(express.json());
 
-app.use(logger('dev'));
+app.get("/api/add", (req, res) => {
+  res.status(200).json({
+    status: "success",
+    data: {
+      add: 1 + 1
+    }
+  });
+});
+
+app.post("/api/add", (req, res) => {
+  const num1 = req.body.number1;
+  const num2 = req.body.number2;
+  const sumTotal = num1 + num2;
+
+  res.status(200).json({
+    status: "success",
+    data: {
+      add: sumTotal
+    }
+  });
+});
+// view engine setup
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "jade");
+
+app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, "public")));
 
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
+app.use("/", indexRouter);
+app.use("/users", usersRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
@@ -31,11 +54,15 @@ app.use(function(req, res, next) {
 app.use(function(err, req, res, next) {
   // set locals, only providing error in development
   res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
+  res.locals.error = req.app.get("env") === "development" ? err : {};
 
   // render the error page
   res.status(err.status || 500);
-  res.render('error');
+  res.render("error");
 });
+
+// app.get("/api/v1/add", (req, res) => {
+//   res.status(200).json({ message: "hello from the server " });
+// });
 
 module.exports = app;
